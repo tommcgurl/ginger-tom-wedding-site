5961734558c1eef133b39bd10e2ca260dcdc6504
Tom McGurl <Tom.Mcgurl@gmail.com>
2016-09-03 16:55:04 -0400

    Working tabs and hash-history

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-class Content {
-  constructor() {
+import MainContent from './components/MainContent';
+import Tabs from './components/Tabs';
 
+// Get the initial route
+const bootStrapApp = () => {
+  const currentTab = window.location.hash ? window.location.hash.toUpperCase().substring(1) : 'HOME';
+
+  // Set the intial active tab
+  let tabs = document.getElementsByClassName('tab-item');
+  // We cannot use for each since tabs is not a traditional array
+  for (let i = 0; i < tabs.length; i ++) {
+    const innerText = tabs[i].innerText.replace(/\W/g, '');
+    if (innerText === currentTab) {
+      // Grab the 'a' tag under the 'li' tag
+      tabs[i].children[0].classList = ['active'];
+    } else {
+      tabs[i].children[0].classList = [];
+    }
   }
 
-  render() {
-    return (
-      `
-      <div> The content goes here. </div>
-      `
-    );
-  }
+  const ContentComponent = new MainContent('content', currentTab);
+  ContentComponent.render();
+
+  // Create event listener on the tabs
+  const tabList = document.getElementById('tab-list');
+  tabList.addEventListener("click", e => ContentComponent.handleTabChange(e));
 }
+
+bootStrapApp();
