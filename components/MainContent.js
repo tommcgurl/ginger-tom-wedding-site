e5aa553c0ca23b6ae208a6a9a7535f5ed4f3df7f
Tom McGurl <thomas@chartbeat.com>
2016-09-25 16:15:22 -0400

    Added names to fixed header. Mobile image adjustments.

--- components/MainContent.js
+++ components/MainContent.js
@@ -29,31 +29,36 @@ export default class Content {
     let mainContent;
     let topImageSource;
     let bottomImageSource;
-
+    let topImageClass;
     switch(this.currentTab) {
       case HOME:
         mainContent = `<h1 class="second-header">Website coming soon    :)</h1>`;
         topImageSource = "images/main1.jpg";
+        topImageClass = "main-image"
         bottomImageSource = "images/main3.jpg";
         break;
       case PHOTOS:
         mainContent = `<h1 class="second-header">Website coming soon    :)</h1>`
         topImageSource = "images/main1.jpg";
+        topImageClass = "main-image";
         bottomImageSource = "images/main3.jpg";
         break;
       case LOCATION:
         mainContent = `<h1 class="second-header">Website coming soon    :)</h1>`
         topImageSource = "images/mpinn.jpg";
+        topImageClass = "location-image";
         bottomImageSource = "images/main3.jpg";
         break;
       case BRIDAL_PARTY:
         mainContent = `<h1 class="second-header">Website coming soon    :)</h1>`
         topImageSource = "images/main1.jpg";
+        topImageClass = "main-image";
         bottomImageSource = "images/main3.jpg";
         break;
       case REGISTRY:
         mainContent = `<h1 class="second-header">Website coming soon    :)</h1>`
         topImageSource = "images/main1.jpg";
+        topImageClass = "main-image";
         bottomImageSource = "images/main3.jpg";
         break;
     }
@@ -73,7 +78,8 @@ export default class Content {
     return {
       mainContent,
       topImageSource,
-      bottomImageSource
+      bottomImageSource,
+      topImageClass
     };
   }
 
@@ -81,8 +87,12 @@ export default class Content {
     const {
       mainContent,
       topImageSource,
-      bottomImageSource
+      bottomImageSource,
+      topImageClass
     } = this.getContent();
+    let topImageElement = document.getElementById(this.topImageSelector);
+    topImageElement.src = topImageSource;
+    topImageElement.classList = [topImageClass];
     document.getElementById(this.selector).innerHTML = mainContent;
     document.getElementById(this.topImageSelector).src = topImageSource;
     document.getElementById(this.bottomImageSelector).src = bottomImageSource;
