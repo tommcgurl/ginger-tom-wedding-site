d65a4ad02e708cef3c78b5d9b302a1116f09a287
Tom McGurl <thomas@chartbeat.com>
2017-01-24 07:36:26 -0500

    Fixes for firefox. Added header to sections on Bridal Party page.

--- components/BridalParty.js
+++ components/BridalParty.js
@@ -1,7 +1,7 @@
 import BridalPartyBio from './BridalPartyBio';
 import './BridalParty.css';
 
-const BRIDAL_PARTY = [
+const BRIDES_MAIDS = [
   {
     name: 'Lana Berger',
     imagePath: '../images/bridal-party/Lana.jpg',
@@ -31,7 +31,9 @@ const BRIDAL_PARTY = [
     name: 'Lindsey Saks',
     imagePath: '../images/bridal-party/Lindsey.jpg',
     bio: 'Placeholder text for now. Fill this in with a short bio about the bridal party memeber. Should be about three to four sentances for now.'
-  },
+  }
+]
+const GROOMSMAN = [
   {
     name: 'Jimmy Joe McGurl',
     imagePath: '../images/bridal-party/Lana.jpg',
@@ -62,7 +64,6 @@ const BRIDAL_PARTY = [
     imagePath: '../images/bridal-party/Lindsey.jpg',
     bio: 'Placeholder text for now. Fill this in with a short bio about the bridal party memeber. Should be about three to four sentances for now.'
   }
-
 ];
 
 export default class BridalParty {
@@ -70,8 +71,8 @@ export default class BridalParty {
     this.selector = selector;
   }
 
-  renderBios() {
-    return BRIDAL_PARTY.reduce((prev, person) => {
+  renderBios(group) {
+    return group.reduce((prev, person) => {
       return prev + BridalPartyBio(person.name, person.imagePath, person.bio);
     }, ``);
   }
@@ -80,8 +81,27 @@ export default class BridalParty {
     return {
       main:   (
         `
-          <div class="bridal-party-container">
-            ${this.renderBios()}
+          <div class="bridal-party-outer-container">
+            <div class="brides-maids header-with-decoration">
+              <div class="line-decoration"></div>
+              <div class="secondary-header">
+                Brides Maids
+              </div>
+              <div class="line-decoration"></div>
+            </div>
+            <div class="bridal-party-container">
+              ${this.renderBios(BRIDES_MAIDS)}
+            </div>
+            <div class="groomsman header-with-decoration">
+              <div class="line-decoration"></div>
+              <div class="secondary-header">
+                Groomsman
+              </div>
+              <div class="line-decoration"></div>
+            </div>
+            <div class="bridal-party-container">
+              ${this.renderBios(GROOMSMAN)}
+            </div>
           </div>
         `
       ),
