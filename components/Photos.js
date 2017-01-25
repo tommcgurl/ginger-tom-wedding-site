31ff2d9ace0a1dc4cacb8d923e3dd32204385cf9
Tom McGurl <thomas@chartbeat.com>
2017-01-25 08:37:03 -0500

    Adding more photos.

--- components/Photos.js
+++ components/Photos.js
@@ -22,6 +22,14 @@ export default class Photos {
             <img class="photo" src="../images/photos/10.jpg" />
             <img class="photo" src="../images/photos/11.jpg" />
             <img class="photo" src="../images/photos/12.jpg" />
+            <img class="photo verticle" src="../images/photos/13.jpg" />
+            <img class="photo verticle" src="../images/photos/14.jpg" />
+            <img class="photo verticle" src="../images/photos/15.jpg" />
+            <img class="photo verticle" src="../images/photos/16.jpg" />
+            <img class="photo verticle" src="../images/photos/17.jpg" />
+            <img class="photo verticle" src="../images/photos/18.jpg" />
+            <img class="photo verticle" src="../images/photos/19.jpg" />
+            <img class="photo verticle" src="../images/photos/20.jpg" />
           </div>
         `
       ),
